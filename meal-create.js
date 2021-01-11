ae062482de4fd611ac178f6402fb5490dc1d21a4
James Francis <james.franics@madetech.com>
2021-01-11 12:32:42 +0000

    altering the meal create file to start adding push functionality

--- meal-create.js
+++ meal-create.js
@@ -1,19 +1,45 @@
 "use strict";
 import * as AWS from "aws-sdk";
 
-module.exports.addMeal = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "This will be our add meal function - Emmas addition ",
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+AWS.config.setPromisesDependency(require("bluebird"));
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+const submitMeal = (meal) => {
+  console.log("Submitting meal");
+  const mealInfo = {
+    TableName: process.env.MEAL_TABLE,
+    Item: meal,
+  };
+  return dynamoDb
+    .put(mealInfo)
+    .promise()
+    .then((res) => meal);
 };
+
+const mealInfo = (mealName, ingredients) => {
+  const timestamp = new Date().getTime();
+  return {
+    mealName,
+    ingredients,
+    submittedAt: timestamp,
+    updatedAt: timestamp,
+  };
+};
+
+// module.exports.addMeal = async (event) => {
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify(
+//       {
+//         message: "This will be our add meal function - Emmas addition ",
+//         input: event,
+//       },
+//       null,
+//       2
+//     ),
+//   };
+
+//   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+//   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+// };
